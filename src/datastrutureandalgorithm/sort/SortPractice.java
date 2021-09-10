7495266b1b222cfc6ae477bd02f122ce76d2bb03
Masudurrshiblee <mrshiblee24@gmail.com>
2021-09-10 11:16:28 -0400

    apache poi is done

--- src/datastrutureandalgorithm/sort/SortPractice.java
+++ src/datastrutureandalgorithm/sort/SortPractice.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class SortPractice {
     public static void main(String args[]){
+
         int [] stId={1,7,6,3,2,4,5};
         Arrays.sort(stId);
         System.out.println(Arrays.toString(stId));
