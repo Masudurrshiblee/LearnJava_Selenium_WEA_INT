60e00a4657503cf7494c0ffe9f75f61fcbb7a45e
Masudurrshiblee <mrshiblee24@gmail.com>
2021-09-09 23:31:46 -0400

    sort practice commited

---
+++ src/datastrutureandalgorithm/sort/SortPractice.java
@@ -0,0 +1,11 @@
+package datastrutureandalgorithm.sort;
+
+import java.util.Arrays;
+
+public class SortPractice {
+    public static void main(String args[]){
+        int [] stId={1,7,6,3,2,4,5};
+        Arrays.sort(stId);
+        System.out.println(Arrays.toString(stId));
+    }
+}
